cf54ad919e4dcd6980e97ae683a85757cddcc377
Nicolás <nicolas@unergy.io>
2023-05-26 11:00:14 -0500

    Corrección de protocolo BGW con parties 'locales'. Ya está listo para implementación 'completa'

--- src/math_example/main.cc
+++ src/math_example/main.cc
@@ -4,31 +4,16 @@
 
 using namespace scl;
 using namespace std;
-using Field = math::Fp<61>;
-
-template <typename T>
-T RandomNonZero(util::PRG& prg) {
-    auto a = T::Random(prg);
-    for (std::size_t i = 0; i < 10; ++i) {
-        if (a == T::Zero()) {
-            a = T::Random(prg);
-        }
-        break;
-    }
-    if (a == T::Zero()) {
-        throw std::logic_error("could not generate a non-zero random value");
-    }
-    return a;
-}
+using Mersenne61 = math::Fp<61>;
 
 int main() {
     auto prg = util::PRG::Create("seed");
-    Field zero = Field::Zero();
-    Field one = Field::One();
-    auto nz = RandomNonZero<Field>(prg);
-    auto nz_plus_one = one + nz;
-    auto nz_plus_zero = zero + nz;
-    cout << nz << '\n';
+    Mersenne61 zero = Mersenne61::Zero();
+    Mersenne61 one = Mersenne61::One();
+    auto random = Mersenne61::Random(prg);
+    auto nz_plus_one = one + random;
+    auto nz_plus_zero = zero + random;
+    cout << random << '\n';
     cout << nz_plus_zero << '\n';
     cout << nz_plus_one << '\n';
 }
